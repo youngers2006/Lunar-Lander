69dd46e629d0d1b0139ba67007c9a75711316b83
youngers2006 <sy2224@ic.ac.uk>
2025-08-27 00:46:00 +0100

    added

--- PPO.py
+++ PPO.py
@@ -1,77 +1,44 @@
 import gymnasium as gym
-import jax 
-import jax.numpy as jnp
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
-from jax.experimental import host_callback as hcb
-import jax.nn as nn
-import optax
-import flax.nnx as nnx
-import distrax
 import math
 from tqdm import tqdm
 
-class JaxGymWrapper:
-    def __init__(self, env_name, **kwargs):
-        self._env = gym.make(env_name, **kwargs)
-        self._step_result_shape = (
-            jax.ShapeDtypeStruct(self._env.observation_space.shape, self._env.observation_space.dtype),
-            jax.ShapeDtypeStruct((), np.float32),
-            jax.ShapeDtypeStruct((), np.bool_),
-            jax.ShapeDtypeStruct((), np.bool_)
-        )
-        self._reset_result_shape = jax.ShapeDtypeStruct(
-            self._env.observation_space.shape, self._env.observation_space.dtype
-        )
-
-    def _host_step(self, action):
-        obs, reward, terminated, truncated, info = self._env.step(np.asarray(action))
-        return obs, reward, terminated, truncated
-
-    def _host_reset(self):
-        obs, info = self._env.reset()
-        return obs
-    
-    def step(self, action):
-        return hcb.call(self._host_step, action, result_shape=self._step_result_shape)
-
-    def reset(self):
-        return hcb.call(self._host_reset, (), result_shape=self._reset_result_shape)
-    
-    def close(self):
-        self._env.close()
-
-class Critic(nnx.Module):
+class Critic(nn.Module):
     # critic takes state and return value
     def __init__(self, state_size, hidden_size, rngs):
-        self.L1 = nnx.Linear(state_size, hidden_size, rngs)
-        self.L2 = nnx.Linear(hidden_size, 1, rngs)
+        super.__init__()
+        self.L1 = nn.Linear(state_size, hidden_size, rngs)
+        self.L2 = nn.Linear(hidden_size, 1, rngs)
     
-    def __call__(self, x):
+    def forward(self, x):
         x = self.L1(x)
-        x = nnx.relu(x)
+        x = nn.ReLU(x)
         x = self.L2(x)
         return x
 
-class Actor(nnx.Module):
-    def __init__(self, state_size, hidden_size, action_space_size, rngs):
-        self.L1 = nnx.Linear(state_size, hidden_size, rngs=rngs)
-        self.L2 = nnx.Linear(hidden_size, action_space_size * 2, rngs=rngs)
+class Actor(nn.Module):
+    def __init__(self, state_size, hidden_size_1, hidden_size_2, action_space_size):
+        super.init()
+        self.L1 = nn.Linear(state_size, hidden_size_1)
+        self.L2 = nn.Linear(hidden_size_1, hidden_size_2)
+        self.mu = nn.Linear(hidden_size_2, action_space_size)
+        self.log_std = nn.Linear(hidden_size_2, action_space_size)
 
-    def __call__(self, x):
+    def forward(self, x):
         x = self.L1(x)
-        x = nnx.relu(x)
+        x = nn.ReLU(x)
         x = self.L2(x)
-        mean, log_std = jnp.split(
-            x, 
-            2, 
-            axis=-1
-        )
-        std_dev = jnp.exp(log_std)
+        x = nn.ReLU(x)
+        mean = self.mu(x)
+        std_dev = torch.exp(self.log_std(x))
         return mean, std_dev
 
     def act(self, state, key):
-        mean, std_dev = self(state)
-        action_distribution = distrax.Normal(loc=mean, scale=std_dev) 
+        mean, std_dev = self.forward(state)
+        action_distribution = torch.distributions.Normal(loc=mean, scale=std_dev) 
         action, log_prob = action_distribution.sample_and_log_prob(seed=key)
         return action, jnp.sum(log_prob, axis=-1)
     
@@ -83,12 +50,6 @@ class ModelState(nnx.Object):
         self.critic_GD = critic_GD
         self.critic_params = critic_params
         self.critic_state = critic_state
-
-def policy_rollout(T):
-    env.reset(seed)
-    for t in T:
-
-    return data_dict
 
 def compute_advantages():
     return advantages
@@ -117,25 +78,26 @@ def surrogate_optimisation_step(actor_GD, actor_params, actor_state, critic_GD, 
 
 def main():
 
-    @jax.jit
     def policy_rollout(T, seed, ):
-        state, info = jax_env.step(seed)
+        data_buffer = 
+        state, info = env.reset(seed)
         terminated = False
         truncated = False
         for t in T:
-            action = 
-            state_, reward, terminated, truncated, info = jax_env(action)
+            action = actor()
+            state_, reward, terminated, truncated, info = env.step(action)
 
             state = state_
-        
-            if terminated:
+            if terminated or truncated:
                 seed += 1
-                state, info = jax_env_reset(seed)
+                state, info = env.reset(seed)
+                terminated = False
+                truncated = False
 
         return data_dict
 
     env_id = 'LunarLander-V3'
-    jax_env = JaxGymWrapper(env_id, continuous=True)
+    env = gym.make(env_id, continuous=True)
 
     seed = 43
     iterations = 500
