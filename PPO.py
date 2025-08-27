21cc2bce5bb11e240fed8d082c5045df338153db
youngers2006 <sy2224@ic.ac.uk>
2025-08-27 19:31:26 +0100

    added

--- PPO.py
+++ PPO.py
@@ -29,15 +29,26 @@ class DataSet:
         self.values[self.t] = pred_value
         self.t += 1
 
-    def compute_advantage_and_returns(self, lambda_, gamma):
-        adv = 0
+    def compute_advantage_and_returns(self, critic, lambda_, gamma):
+        adv_ = 0
+        with torch.no_grad():
+            value_ = critic.forward(self.state[self.t]) * (1 - self.dones[self.t])
+            R = critic.forward(self.state[self.t])
+
         for i in reversed(range(self.t)):
-            mask = 1.0 - self.dones[i]
-            delta = self.rewards[i] + gamma * last_value * mask - self.pred_values[i]
-            adv = delta + gamma * lambda_ * mask * adv
-            self.advantages[i] = adv
-            last_value = self.pred_values[i]
-        self.returns[:self.t] = self.advantages[:self.t] + self.values[:self.t]
+            if self.dones[i]:
+                value_ = 0
+                R = 0
+                TD_error = self.reward[i] + gamma * value_ - self.values[i]
+                adv = TD_error
+            else:
+                TD_error = self.reward[i] + gamma * value_ - self.values[i]
+                adv = TD_error + gamma * lambda_ * adv_ 
+            adv_ = adv
+            R = self.rewards[i] 
+            value_ = self.values[i]
+            return_ = self.rewards[i] + gamma * R
+
 
     def get_minibatches(self, batch_size):
         idxs = np.arange(self.t)
@@ -50,24 +61,26 @@ class DataSet:
                 self.actions[batch_idx],
                 self.returns[batch_idx],
                 self.advantages[batch_idx],
-                self.log_probs[batch_idx]
+                self.log_probs[batch_idx],
+                self.pred_values[batch_idx]
             )
 
     def reset(self):
         self.t = 0
 
-
 class Critic(nn.Module):
-    def __init__(self, state_size, hidden_size, rngs):
+    def __init__(self, state_size, hidden_size_1, hidden_size_2):
         super.__init__()
-        self.L1 = nn.Linear(state_size, hidden_size, rngs)
-        self.L2 = nn.Linear(hidden_size, 1, rngs)
+        self.L1 = nn.Linear(state_size, hidden_size_1)
+        self.L2 = nn.Linear(hidden_size_1, hidden_size_2)
+        self.L3 = nn.Linear(hidden_size_2, 1)
     
     def forward(self, x):
         x = self.L1(x)
         x = nn.ReLU(x)
         x = self.L2(x)
-        return x
+        x = nn.ReLU(x)
+        return self.L3(x)
 
 class Actor(nn.Module):
     def __init__(self, state_size, hidden_size_1, hidden_size_2, action_space_size):
@@ -85,147 +98,149 @@ class Actor(nn.Module):
         mean = self.mu(x)
         std_dev = torch.exp(self.log_std(x))
         return mean, std_dev
-
-    def act(self, state, key):
+    
+    def get_dist(self, state):
         mean, std_dev = self.forward(state)
-        action_distribution = torch.distributions.Normal(loc=mean, scale=std_dev) 
+        return torch.distributions.Normal(loc=mean, scale=std_dev)
+
+    def act(self, state):
+        action_distribution = self.get_dist(state)
         action = action_distribution.rsample()
         log_prob = action_distribution.log_prob(action).sum(dim=-1, keepdim=True)
         return action, log_prob
 
-def compute_advantages():
-    return advantages
+def ppo_update(actor, critic, dataset, actor_optimizer, critic_optimizer, batch_size, BETA, clip_eps=0.2):
+    for states, actions, returns, advantages, old_log_probs, values in dataset.get_minibatches(batch_size):
 
-def getImportanceRatio():
-    return importance_ratio
+        dist = actor.get_dist(states)
+        new_log_probs = dist.log_prob(actions).sum(dims=-1, keepdim=True)
+        ratios = (new_log_probs - old_log_probs).exp()
 
-def surrogate_obj(advantage, importance_ratio, Value_pred, Value_target, action_dist_std_dev,*, epsilon, C1, C2):
-    L_clip = jnp.min(jnp.array([
-        importance_ratio * advantage, 
-        jnp.clip(importance_ratio, 1 - epsilon, 1 + epsilon) * advantage
-        ]), 
-        axis=-1
-    )
-    L_vf = jnp.power(Value_pred - Value_target, 2) 
-    entropy = 0.5 * (jnp.log(2 * math.pi * (action_dist_std_dev ** 2) + 1))
-    return L_clip - C1 * L_vf + C2 * entropy 
+        surrogate1 = ratios * advantages
+        surrogate2 = torch.clamp(ratios, 1 - clip_eps, 1 + clip_eps) * advantages
+        actor_loss = -torch.min(surrogate1, surrogate2).mean()
+        actor_loss = actor_loss - BETA 
 
-def surrogate_optimisation_step(actor_GD, actor_params, actor_state, critic_GD, critic_params, critic_state, opt_state_actor, opt_state_critic):
+        value_loss = (critic(states) - returns).pow(2).mean()
 
-    def surrogate_obj_call():
-        return 0 
-    
-    return new_params_Critic, new_paramsActor, new_stateCritic, new_stateActor, new_opt_stateActor, new_opt_stateCritic
+        actor_optimizer.zero_grad()
+        actor_loss.backward()
+        actor_optimizer.step()
+
+        critic_optimizer.zero_grad()
+        value_loss.backward()
+        critic_optimizer.step()
+
+def policy_rollout(env, actor, critic, dataset, seed):
+        state, _ = env.reset(seed)
+        done = False
+        for t in range(dataset.T):
+            action, log_prob = actor.act(state)
+            state_, reward, terminated, truncated, _ = env.step(action)
+            done = (terminated or truncated)
+            value = critic(state)
+            dataset.add(
+                state,
+                action,
+                reward,
+                value,
+                done,
+                log_prob
+            )
+            state = state_
+            if done:
+                seed += 1
+                state, _ = env.reset(seed)
+                done = False
 
 def main():
-
-    def policy_rollout(T, seed, ):
-        data_buffer = 
-        state, info = env.reset(seed)
-        terminated = False
-        truncated = False
-        for t in T:
-            action = actor()
-            state_, reward, terminated, truncated, info = env.step(action)
-
-            state = state_
-            if terminated or truncated:
-                seed += 1
-                state, info = env.reset(seed)
-                terminated = False
-                truncated = False
-
-        return data_dict
+    if torch.cuda.is_available():
+        device = 'cuda'
+        print("cuda selected as device")
+    else:
+        device = 'cpu'
+        print("cpu selected as device")
 
     env_id = 'LunarLander-V3'
     env = gym.make(env_id, continuous=True)
 
     seed = 43
     iterations = 500
-    epochs = 100
-    T = 50
+    epochs = 20
+    T = 600
     N = 1
-    hs_c = 16
-    hs_a = 16
+    hs_c1 = 16
+    hs_c2 = 16
+    hs_a1 = 16
+    hs_a2 = 16
+
     Learn_Rate_c = 0.001
     beta_1_c = 0.999
     beta_2_c = 0.9
     Learn_Rate_a = 0.001
     beta_1_a = 0.999
     beta_2_a = 0.9
+    gamma = 0.999
+    lambda_ = 0.8
+    epsilon = 0.2
+    batch_size = 50
 
     state_size = 8
     action_space_size = 4
 
     critic = Critic(
         state_size, 
-        hs_c, 
-        rngs
+        hs_c1,
+        hs_c2
     )
-
     actor = Actor(
         state_size, 
-        hs_a, 
+        hs_a1, 
+        hs_a2,
+        action_space_size
+    )
+
+    optimiser_actor = torch.optim.Adam(
+        actor.parameters(), 
+        lr=Learn_Rate_a, 
+        betas=(beta_1_a, beta_2_a)
+    )
+    optimiser_critic = torch.optim.Adam(
+        critic.parameters(), 
+        lr=Learn_Rate_c, 
+        betas=(beta_1_c, beta_2_c)
+    )
+
+    dataset = DataSet(
+        state_size, 
         action_space_size, 
-        rngs
-    )
-
-    optimiser_critic = optax.adam(
-        learning_rate=Learn_Rate_c, 
-        b1=beta_1_c, 
-        b2=beta_2_c
-    )
-
-    optimiser_actor = optax.adam(
-        learning_rate=Learn_Rate_a, 
-        b1=beta_1_a, 
-        b2=beta_2_a
-    )
-
-    graph_defCritic, paramsCritic, stateCritic = nnx.split(critic, nnx.Param, nnx.State)
-    graph_defActor, paramsActor, stateActor = nnx.split(critic, nnx.Param, nnx.State)
-
-    opt_stateActor = optimiser_actor.init(paramsActor)
-    opt_stateCritic = optimiser_critic.init(paramsCritic)
-
-    current_state = ModelState(
-        graph_defCritic, 
-        paramsCritic, 
-        stateCritic, 
-        graph_defActor, 
-        paramsActor, 
-        stateActor, 
-    )
-
-    old_state = ModelState(
-        graph_defCritic, 
-        paramsCritic, 
-        stateCritic, 
-        graph_defActor, 
-        paramsActor, 
-        stateActor, 
+        T, 
+        device
     )
 
     for iteration in range(iterations):
-        rollout_data = policy_rollout()
-        advantages = compute_advantages()
-        data_batches = batch_data()
+        policy_rollout(
+            env, 
+            actor, 
+            critic, 
+            dataset, 
+            seed
+        )
+        dataset.compute_advantage_and_returns(
+            lambda_,
+            gamma
+        )
         for epoch in range(epochs):
-            for batch in tqdm(data_batches, desc=f'epoch {epoch}/{epochs} in iteration {iteration}/{iterations}', leave=False):
-                importance_ratio = getImportanceRatio()
-                new_params_Critic, new_paramsActor, new_stateCritic, new_stateActor, new_opt_stateActor, new_opt_stateCritic = surrogate_optimisation_step()
-                current_state.actor_params = new_paramsActor
-                current_state.actor_state = new_stateActor
-                current_state.critic_params = new_params_Critic
-                current_state.critic_state = new_stateCritic
-                opt_stateActor = new_opt_stateActor
-                opt_stateCritic = new_opt_stateCritic
-
-        old_state.actor_params = current_state.actor_params 
-        old_state.actor_state = current_state.actor_state
-        old_state.critic_params = current_state.critic_params
-        old_state.critic_state = current_state.critic_state
-
+            ppo_update(
+                actor,
+                critic,
+                dataset,
+                optimiser_actor,
+                optimiser_critic,
+                batch_size,
+                epsilon
+            )
+                
 if __name__ == "__main__":
     print("Running Training...")
     main()
