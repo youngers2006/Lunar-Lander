aa16f71c1cf37a98cce14b5542508e18fa69d29c
youngers2006 <sy2224@ic.ac.uk>
2025-09-06 17:02:31 +0100

    added

--- MPC.py
+++ MPC.py
@@ -7,38 +7,108 @@ import math
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-class dynamics_model:
-    def __init__(self, state_size, action_size, hidden1_size, hidden2_size):
-        self.layer1 = nn.Linear(in_features=(state_size + action_size), out_features=hidden1_size)
-        self.layer2 = nn.Linear(in_features=hidden1_size, out_features=hidden2_size)
-        self.layer3 = nn.Linear(in_features=hidden2_size, out_features=(state_size))
+class DataSet:
+    pass
 
-    def forward(self, x):
-        x = self.layer1(x)
-        x = nn.SiLU(x)
-        x = self.layer2(x)
-        x = nn.SiLU(x)
-        return self.layer3(x)
-
-class reward_model:
+class DynamicsModel:
     def __init__(self, state_size, action_size, hidden1_size, hidden2_size):
-        self.layer1 = nn.Linear(in_features=(state_size + action_size), out_features=hidden1_size)
-        self.layer2 = nn.Linear(in_features=hidden1_size, out_features=hidden2_size)
-        self.layer3 = nn.Linear(in_features=hidden2_size, out_features=(1,))
+        self.shared_net = nn.Sequential(
+            nn.Linear(in_features=(state_size + action_size), out_features=hidden1_size),
+             nn.SiLU(),
+            nn.Linear(in_features=hidden1_size, out_features=hidden2_size),
+            nn.SiLU()
+        )
+        self.mu_head = nn.Linear(in_features=hidden2_size, out_features=state_size)
+        self.log_sigma_head = nn.Linear(in_features=hidden2_size, out_features=state_size)
+
+    def forward(self, state, action):
+        x = torch.cat(
+            tensors=[state, action],
+            dim=-1
+        )
+        x = self.shared_net(x)
+        mu = self.mu_head(x)
+        log_sigma = torch.clamp(
+            input=self.log_sigma_head(x),
+            max=2,
+            min=-10
+        )
+        sigma = torch.exp(log_sigma)
+
+        return mu, sigma
     
-    def forward(self, x):
-        x = self.layer1(x)
-        x = nn.SiLU(x)
-        x = self.layer2(x)
-        x = nn.SiLU(x)
-        return self.layer3(x)
+    def next_state(self, state, action):
+        mu, sigma = self.forward(state, action)
+        state_dist = torch.distributions.Normal(
+            loc=mu, 
+            scale=sigma
+        )
+        delta_state = state_dist.rsample()
+        next_state_prediction = state + delta_state
+        return next_state_prediction
+
+
+class RewardModel:
+    def __init__(self, state_size, action_size, hidden1_size, hidden2_size):
+        super().__init__()
+        self.reward_net = nn.Sequential(
+            nn.Linear(in_features=(state_size + action_size), out_features=hidden1_size),
+            nn.SiLU(),
+            nn.Linear(in_features=hidden1_size, out_features=hidden2_size),
+            nn.SiLU(),
+            nn.Linear(in_features=hidden2_size, out_features=1)
+        )
+    
+    def forward(self, state, action):
+        x = torch.cat(
+            tensors=[state, action],
+            dim=-1
+        )
+        return self.reward_net(x)
 
 class MPC:
-    def __init__(self, ):
+    def __init__(self, planner, planning_horizon):
+        self.planning_algorithm = planner
+        self.horizon = planning_horizon
+
+    def act(self):
+        self.planning_algorithm.plan(self.horizon)
 
 class iLQR:
-    def __init__(self, ):
+    def __init__(self):
+        pass
+
+    def linearise(self, )
+
+    def plan(self, planning_horizon, dynamics_model, reward_model):
+        pass
 
 
 
 def main():
+    if torch.cuda.is_available():
+        device = 'cuda'
+        print("cuda selected as device")
+    else:
+        device = 'cpu'
+        print("cpu selected as device")
+
+    env_id = 'LunarLanderContinuous-v3'
+    env = gym.make(env_id)
+
+    data_set = DataSet()
+
+    dynamics_model = DynamicsModel()
+    reward_model = RewardModel()
+
+    iLQR_planner = iLQR()
+    MPC_controller = MPC(
+        planner=iLQR_planner,
+    )
+
+
+
+if __name__ == "__main__":
+    print("running training")
+    main()
+    print("training complete")
